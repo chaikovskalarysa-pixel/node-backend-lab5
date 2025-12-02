8950f6b5f4b7fd05c42cc26bc4b0a537e8d7c31e
chaikovskalarysa-pixel <chaikovska.larysa@chnu.edu.ua>
2025-12-02 19:48:04 +0200

    Update db.js

--- services/db.js
+++ services/db.js
@@ -1,10 +1,20 @@
 import mongoose from "mongoose";
-export const connectDB = async (mongoURI) => {
- try {
- await mongoose.connect(mongoURI);
- console.log("MongoDB connected");
- } catch (err) {
- console.error("Mongo error:", err);
- throw err;
- }
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const connectDB = async () => {
+  const uri = process.env.MONGODB_URI;
+
+  if (!uri) {
+    throw new Error("MONGODB_URI не знайдено в .env файлі!");
+  }
+
+  try {
+    await mongoose.connect(uri);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("Mongo error:", err);
+    throw err;
+  }
 };
