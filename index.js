9234872805c30519ae2ff2d37431993136cf2301
chaikovskalarysa-pixel <chaikovska.larysa@chnu.edu.ua>
2025-12-02 22:37:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,29 @@ import { connectDB } from "./services/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 
-dotenv.config(); 
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Додаємо цей рядок — головна сторінка
+app.get("/", (req, res) => {
+  res.json({ message: "MyStore API працює!", time: new Date().toISOString() });
+});
+
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 
-await connectDB(process.env.MONGODB_URI);
-
+// Підключення до MongoDB
+connectDB().catch(err => {
+  console.error("Не вдалося підключитися до MongoDB", err);
+  process.exit(1);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on https://node-backend-lab5.onrender.com`);
+  console.log(`PORT: ${PORT}`);
 });
-
